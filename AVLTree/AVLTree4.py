22d9640a25dfe310f3d06c424950a7e8469f142c
Anantha250 <ananthaxb@gmail.com>
2026-01-20 13:50:50 +0700

    opmitize_cut_tree

--- AVLTree/AVLTree4.py
+++ AVLTree/AVLTree4.py
@@ -1,120 +1,133 @@
 class Node:
-    def __init__(self, data, left = None, right = None):
+    def __init__(self, data, left=None, right=None):
         self.data = data
-        self.left = None if left is None else left
-        self.right = None if right is None else right
-        self.height = self.setHeight()
+        self.left = left
+        self.right = right
+        self.height = 0
+        self.setHeight()
 
     def __str__(self):
         return str(self.data)
 
+    def getHeight(self, node):
+        return -1 if node is None else node.height
+
     def setHeight(self):
-        a = self.getHeight(self.left)
-        b = self.getHeight(self.right)
-        self.height = 1 + max(a,b)
+        self.height = 1 + max(self.getHeight(self.left),
+                              self.getHeight(self.right))
         return self.height
 
-    def getHeight(self, node):
-        return -1 if node == None else node.height
-
-    def balanceValue(self):      
+    def balanceValue(self):
         return self.getHeight(self.right) - self.getHeight(self.left)
 
-    def _gen_display(self) -> 'tuple[list, int, int, int]':
+    def _gen_display(self):
         if self is None:
             return [], 0, 0, 0
+
         lt, lf, lv, lb = Node._gen_display(self.left)
         rt, rf, rv, rb = Node._gen_display(self.right)
         data = str(self.data)
+
         if not lt and not rt:
             return [data], 0, len(data), 0
+
         add_left, add_right = int(bool(lt)), int(bool(rt))
-        line = ((' '*(lf+lv) + '/' + ' '*(lb)) * add_left +
+
+        line = ((' ' * (lf + lv) + '/' + ' ' * lb) * add_left +
                 ' ' * len(data) +
-                (' '*rf + '\\' + ' '*(rv+rb)) * add_right)
-        out = [' '*(lf+lv+add_left) + '_'*lb + data +
-               '_'*rf + ' '*(rv+rb+add_right), line]
+                (' ' * rf + '\\' + ' ' * (rv + rb)) * add_right)
+
+        out = [' ' * (lf + lv + add_left) + '_' * lb + data +
+               '_' * rf + ' ' * (rv + rb + add_right), line]
+
         if len(lt) > len(rt):
-            rt.extend([' ' * (rf+rv+rb)] * (len(lt) - len(rt)))
-        elif len(lt) < len(rt):
-            lt.extend([' ' * (lf+lv+lb)] * (len(rt) - len(lt)))
+            rt.extend([' ' * (rf + rv + rb)] * (len(lt) - len(rt)))
+        elif len(rt) > len(lt):
+            lt.extend([' ' * (lf + lv + lb)] * (len(rt) - len(lt)))
+
         for l, r in zip(lt, rt):
-            out.append(l + ' '*(len(data)+add_left+add_right) + r)
-        return out, (lf+lv+lb+add_left), len(data), (rf+rv+rb+add_right)
+            out.append(l + ' ' * (len(data) + add_left + add_right) + r)
+
+        return out, (lf + lv + lb + add_left), len(data), (rf + rv + rb + add_right)
+
 
 class AVLTree:
-    def __init__(self, root = None):
-        self.root = root if root else None
+    def __init__(self):
+        self.root = None
 
     def add(self, data):
-        self.root = AVLTree._add(self.root, int(data))
+        self.root = self._add(self.root, int(data))
 
+    @staticmethod
     def _add(root, data):
         if not root:
             return Node(data)
-        elif data < root.data:
+
+        if data < root.data:
             root.left = AVLTree._add(root.left, data)
         else:
             root.right = AVLTree._add(root.right, data)
+
         root.setHeight()
-        balace = root.balanceValue()
-        
-        # Right rotate   right > left
-        if balace > 1 :
+        balance = root.balanceValue()
+
+        if balance > 1:
             if data > root.right.data:
                 return AVLTree.rotateLeft(root)
-            else:
-                root.right = AVLTree.rotateRight(root.right)
-                return AVLTree.rotateLeft(root)
+            root.right = AVLTree.rotateRight(root.right)
+            return AVLTree.rotateLeft(root)
 
-        # Left rotate  left > right
-        if balace < -1 :
+        if balance < -1:
             if data < root.left.data:
                 return AVLTree.rotateRight(root)
-            else:
-                root.left = AVLTree.rotateLeft(root.left)
-                return AVLTree.rotateRight(root)
-        
+            root.left = AVLTree.rotateLeft(root.left)
+            return AVLTree.rotateRight(root)
+
         return root
 
-    def rotateLeft(x) :
+    @staticmethod
+    def rotateLeft(x):
         y = x.right
-        if y == None:
+        if not y:
             return x
+
         x.right = y.left
         y.left = x
         x.setHeight()
         y.setHeight()
         return y
 
-    def rotateRight(x) :
+    @staticmethod
+    def rotateRight(x):
         y = x.left
-        if y == None:
+        if not y:
             return x
+
         x.left = y.right
         y.right = x
         x.setHeight()
         y.setHeight()
         return y
-    
-    def printTree(self):
-        AVLTree._printTree(self.root)
 
-    def _printTree(node , level=0):
-        if not node is None:
+    def printTree(self):
+        self._printTree(self.root)
+
+    @staticmethod
+    def _printTree(node, level=0):
+        if node:
             AVLTree._printTree(node.right, level + 1)
-            print('     ' * level, node.data)
+            print('     ' * level + str(node.data))
             AVLTree._printTree(node.left, level + 1)
 
 
-
 class Tree:
-    def __init__(self, root = None):
-        self.root = root if root else None
+    def __init__(self):
+        self.root = None
 
     def add_left(self, data):
-        self.root = Tree._add_left(self.root, int(data))
-    
+        self.root = self._add_left(self.root, int(data))
+
+    @staticmethod
     def _add_left(root, data):
         if not root:
             return Node(data)
@@ -122,95 +135,98 @@ class Tree:
         return root
 
     def add_right(self, data):
-        self.root = Tree._add_right(self.root, int(data))
-    
+        self.root = self._add_right(self.root, int(data))
+
+    @staticmethod
     def _add_right(root, data):
         if not root:
             return Node(data)
         root.right = Tree._add_right(root.right, data)
         return root
-        
+
     def dfs(self):
         if not self.root:
-            return
+            return []
+
         stack = [self.root]
-        ans = []
+        result = []
+
         while stack:
-            cur = stack.pop()
-            ans.append(cur.data)
-            if cur.left:
-                stack.append(cur.left)
-            if cur.right:
-                stack.append(cur.right)
-        return ans
-    
+            node = stack.pop()
+            result.append(node.data)
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return result
+
     def find_node(self, data):
         cur = self.root
         parent = None
+
         while cur:
             if cur.data == data:
-                return [cur, parent]
-            parent = cur
-            if cur.data < data:
-                cur = cur.right
-            else :
-                cur = cur.left
-        print(f"No {data} in this tree")
-        exit
+                return cur, parent
 
-    def merge_tree(self, parent, data, tree):
-        if not data:
-            return
+            parent = cur
+            if data > cur.data:
+                cur = cur.right
+            else:
+                cur = cur.left
+
+        raise ValueError(f"No {data} in this tree")
+
+    def merge_tree(self, parent, data, subtree):
         if data == self.root.data:
-            self.root = tree.root
-        elif parent.left.data == data:
-            parent.left = tree.root
-        elif  parent.right.data == data:
-            parent.right = tree.root
+            self.root = subtree.root
+        elif parent.left and parent.left.data == data:
+            parent.left = subtree.root
+        elif parent.right and parent.right.data == data:
+            parent.right = subtree.root
 
 
 def top_view(tree):
-    root = tree.root
-    tree_image = root._gen_display()
-    print(*tree_image[0], sep='\n')
-    print("-" * sum(tree_image[1:]))
+    img = tree.root._gen_display()
+    print(*img[0], sep='\n')
+    print("-" * sum(img[1:]))
 
 def top_view_normal(tree):
-    root = tree.root
-    tree_image = root._gen_display()
-    print(*tree_image[0], sep='\n')
+    img = tree.root._gen_display()
+    print(*img[0], sep='\n')
 
 
 data = input("Enter input: ").split(',')
 rotate_node = int(data[0])
 rotate_direct = data[1][0]
-data = data[2].split()
-myTree = AVLTree() 
+values = data[2].split()
+
+myTree = AVLTree()
+
 print("Before")
-for e in data:
-    root = myTree.add(e)
+for v in values:
+    myTree.add(v)
+
 top_view(myTree)
-
-
-
 
 tree = Tree()
 tree.root = myTree.root
 
 cut_tree = Tree()
 cut_tree.root, parent = tree.find_node(rotate_node)
-sort = sorted(cut_tree.dfs())
+sorted_nodes = sorted(cut_tree.dfs())
 
-cut_tree = Tree()
+new_tree = Tree()
+
 if rotate_direct == "l":
-    sort = reversed(sort)
-    for i in sort:
-        cut_tree.add_left(i)
+    for v in reversed(sorted_nodes):
+        new_tree.add_left(v)
 else:
-    for i in sort:
-        cut_tree.add_right(i)
+    for v in sorted_nodes:
+        new_tree.add_right(v)
 
+tree.merge_tree(parent, rotate_node, new_tree)
 
-tree.merge_tree(parent, rotate_node, cut_tree)
 print("After")
 top_view_normal(tree)
